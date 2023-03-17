9408ec06cdb75162b80815379b033b564e3f1dfe
lesalaun <leo.salaun@etudiant.univ-rennes1.fr>
2023-03-17 11:23:56 +0100

    prépa td4

--- TP4/exo1.cpp
+++ TP4/exo1.cpp
@@ -1,52 +1,65 @@
-#include "tp4.h"
-#include "mainwindow.h"
+// #include "tp4.h"
+// #include "mainwindow.h"
 
-#include <QApplication>
-#include <time.h>
-#include <stdio.h>
+// #include <QApplication>
+// #include <time.h>
+// #include <stdio.h>
 
-MainWindow* w = nullptr;
-using std::size_t;
-using std::string;
+// MainWindow* w = nullptr;
+// using std::size_t;
+// using std::string;
 
-int Heap::leftChild(int nodeIndex)
-{
-    return 0;
+void exchange(int array[], int i, int j) {
+    int temp = array[j];
+    array[j] = array[i];
+    array[i] = temp;
 }
 
-int Heap::rightChild(int nodeIndex)
+int leftChild(int * heap, int nodeIndex)
 {
-    return 0;
+    return heap[2*nodeIndex+1];
 }
 
-void Heap::insertHeapNode(int heapSize, int value)
+int rightChild(int * heap, int nodeIndex)
+{
+    return heap[2*nodeIndex+2];
+}
+
+void insertHeapNode(int * heap, int * heapSize, int value)
 {
 	// use (*this)[i] or this->get(i) to get a value at index i
-	int i = heapSize;
+	int i = *heapSize;
+	heap[i] = value;
+	while (i>0 && heap[i] > heap[(i-1)/2]) {
+		exchange(heap,i,(i-1)/2);
+		i = (i-1)/2;
+	}
+	(*heapSize)++;
 }
 
-void Heap::heapify(int heapSize, int nodeIndex)
+void heapify(int * heap, int heapSize, int nodeIndex)
 {
 	// use (*this)[i] or this->get(i) to get a value at index i
 	int i_max = nodeIndex;
 }
 
-void Heap::buildHeap(Array& numbers)
+void buildHeap(int * heap, int * numbers)
 {
 
 }
 
-void Heap::heapSort()
+void heapSort(int * heap)
 {
 
 }
 
 int main(int argc, char *argv[])
 {
-	QApplication a(argc, argv);
-    MainWindow::instruction_duration = 50;
-    w = new HeapWindow();
-	w->show();
+	int * heap;
+	// QApplication a(argc, argv);
+    // MainWindow::instruction_duration = 50;
+    // w = new HeapWindow();
+	// w->show();
 
-	return a.exec();
+	// return a.exec();
 }
