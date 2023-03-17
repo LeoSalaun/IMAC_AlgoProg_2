567acb5b16f08131607766db101ab741264f7321
lesalaun <leo.salaun@etudiant.univ-rennes1.fr>
2023-03-17 15:17:15 +0100

    TP4 - tas

--- TP4/exo1.cpp
+++ TP4/exo1.cpp
@@ -10,6 +10,18 @@
 // using std::string;
 
 
+#include <iostream>
+using namespace std;
+
+void printArray(int array[],int size) {
+    cout << "- ";
+    for (int i=0; i<size; i++)
+    {
+        cout << array[i] << " - ";
+    }
+    cout << endl;
+}
+
 void exchange(int array[], int i, int j) {
     int temp = array[j];
     array[j] = array[i];
@@ -42,21 +54,55 @@ void heapify(int * heap, int heapSize, int nodeIndex)
 {
 	// use (*this)[i] or this->get(i) to get a value at index i
 	int i_max = nodeIndex;
+	for (int i=nodeIndex ; i<2*nodeIndex+1 && i<heapSize ; i++) {
+		if (heap[i] > heap[i_max]) {
+			i_max = i;
+		}
+	}
+
+	if (i_max != nodeIndex) {
+		exchange(heap,nodeIndex,i_max);
+		heapify(heap,heapSize,i_max);
+	}
 }
 
-void buildHeap(int * heap, int * numbers)
+void buildHeap(int * heap, int * heapSize, int * numbers, int size)
 {
+	for (int i=0 ; i<size ; i++) {
+		insertHeapNode(heap,heapSize,numbers[i]);
+	}
 
+	// heap = numbers;
+	// *heapSize = size;
+	// for (int i=0 ; i<size ; i++) {
+	// 	heapify(heap,*heapSize,i);
+	// }
 }
 
-void heapSort(int * heap)
+void heapSort(int * heap, int heapSize)
 {
-
+	for (int i=heapSize-1 ; i>=0 ; i--) {
+		exchange(heap,i,0);
+		heapify(heap,heapSize,i);
+	}
 }
 
 int main(int argc, char *argv[])
 {
-	int * heap;
+	int heap[10];
+	int heapSize = 0;
+	int numbers[10] = {1,58,2,8,4,5,6,54,75,98};
+
+	printArray(numbers,10);
+
+	buildHeap(heap,&heapSize,numbers,10);
+
+	printArray(heap,heapSize);
+
+	heapSort(heap,heapSize);
+
+	printArray(heap,heapSize);
+
 	// QApplication a(argc, argv);
     // MainWindow::instruction_duration = 50;
     // w = new HeapWindow();
