30fba0ceedd624dbedc93c890b90cc166ff6ff88
lesalaun <leo.salaun@etudiant.univ-rennes1.fr>
2023-03-31 12:46:01 +0200

    Début TP5

--- TP5/exo1.cpp
+++ TP5/exo1.cpp
@@ -1,11 +1,21 @@
-#include <tp5.h>
-#include <QApplication>
 #include <time.h>
+#include <vector>
+#include <string>
+#include <iostream>
 
-MainWindow* w = nullptr;
+struct Element
+{
+    std::string value;
+    Element * next;
+
+    Element(std::string value) {
+        this->value = value;
+        this->next = nullptr;
+    }
+};
 
 
-std::vector<string> TP5::names(
+std::vector<std::string> names(
 {
     "Yolo", "Anastasiya", "Clement", "Sirine", "Julien", "Sacha", "Leo", "Margot",
     "JoLeClodo", "Anais", "Jolan", "Marie", "Cindy", "Flavien", "Tanguy", "Audrey",
@@ -14,42 +24,80 @@ std::vector<string> TP5::names(
     "Fanny", "Jeanne", "Elo"
 });
 
-
-int HashTable::hash(std::string element)
+int hash(std::vector<Element*> hash_table, std::string element)
 {
-    // use this->size() to get HashTable size
-    return 0;
+    return (int) element[0] % hash_table.size();
 }
 
-void HashTable::insert(std::string element)
+void insert(std::vector<Element*>* hash_table, std::string element)
 {
     // use (*this)[i] or this->get(i) to get a value at index i
+    if ((*hash_table)[hash(*hash_table, element)] == nullptr) {
+        (*hash_table)[hash(*hash_table, element)] = new Element(element);
+    }
+    else {
+        
+        Element* aux = (*hash_table)[hash(*hash_table, element)];
+        
+        while (aux->next != nullptr) {
+            aux = aux->next;
+        }
+        aux->next = new Element(element);
+        aux->next->value = element;
+    }
 }
 
 /**
  * @brief buildHashTable: fill the HashTable with given names
- * @param table table to fiil
+ * @param table table to fill
  * @param names array of names to insert
  * @param namesCount size of names array
  */
-void buildHashTable(HashTable& table, std::string* names, int namesCount)
+void buildHashTable(std::vector<Element*> hash_table, std::string* names, int namesCount)
 {
-
+    for (int i=0 ; i<31 ; i++) {
+        insert(&hash_table,names[i]);
+    }
 }
 
-bool HashTable::contains(std::string element)
+bool contains(std::vector<Element*> hash_table, std::string element)
 {
-    // Note: Do not use iteration (for, while, ...)
-    return false;
+    if (hash_table[hash(hash_table, element)] == nullptr) {
+        return false;
+    }
+    Element* aux = hash_table[hash(hash_table, element)];
+    
+    while (aux->next != nullptr && aux->value != element) {
+        aux = aux->next;
+    }
+    return aux->value == element;
 }
 
+void printTable(Element * element) {
+    Element * aux = element;
+    while (aux != nullptr) {
+        std::cout << aux->value << " ; ";
+        aux = aux->next;
+    }
+    std::cout << std::endl;
+}
 
 int main(int argc, char *argv[])
 {
-	QApplication a(argc, argv);
-	MainWindow::instruction_duration = 10;
-	w = new HashWindow();
-	w->show();
+    std::vector<Element*> table;
+    table.resize(10);
 
-	return a.exec();
+    for (int i=0 ; i<10 ; i++) {
+        table[i] = nullptr;
+    }
+
+    for (int i=0 ; i<31 ; i++) {
+        insert(&table,names[i]);
+    }
+
+    std::cout << contains(table,"Leo") << std::endl << contains(table,"Lea") << std::endl;
+
+    for (int i=0 ; i<10 ; i++) {
+        printTable(table[i]);
+    }
 }
